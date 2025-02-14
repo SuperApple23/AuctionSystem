6d0c68026ae42e0e0c0ac1521731befff0fd0a70
SuperApple <ntq2372003@gmail.com>
2025-02-14 14:04:48 +0700

    Tạo Area Admin

---
+++ AuctionSystem/Areas/Admin/Controllers/CampaignController.cs
@@ -0,0 +1,29 @@
+﻿using AuctionSystem.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionSystem.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = "Admin")]
+    public class CampaignController : Controller
+    {
+        private readonly AuctionDbContext _context;
+        private readonly ILogger<CampaignController> _logger;
+
+        public CampaignController(AuctionDbContext context, ILogger<CampaignController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var campaigns = _context.Campaigns
+                                    .Include(c => c.SalesMethod)
+                                    .ToList();
+            return View(campaigns);
+        }
+    }
+}
